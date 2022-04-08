e7aee4594d1e0531313a545b5fa7960d07299096
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:31:16 -0500

    chore: create entities product and supplier

---
+++ Autoglass.Data/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Autoglass.Data.Entities
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [DefaultValue(true)]
+        public bool Active { get; set; }
+
+        public DateTime ManufacturingDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public Supplier Supplier { get; set; }
+    }
+}
---
+++ Autoglass.Data/Entities/Supplier.cs
@@ -0,0 +1,9 @@
+﻿namespace Autoglass.Data.Entities
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
