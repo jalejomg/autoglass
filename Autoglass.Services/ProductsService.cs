7b6f8242b725cc454c69e861e336c1581750cd85
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:38:49 -0500

    feat: implement method to create new products

---
+++ Autoglass.Services/ProductsService.cs
@@ -0,0 +1,33 @@
+﻿using Autoglass.Data.Entities;
+using Autoglass.Data.Repositories;
+using Autoglass.Services.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Autoglass.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly IProductsRepository _productsRepository;
+        private readonly IMapper _mapper;
+
+        public ProductsService(
+            IProductsRepository productsRepository,
+            IMapper mapper)
+        {
+            _productsRepository = productsRepository;
+            _mapper = mapper;
+        }
+        public async Task<int> CreateAsync(ProductModel productModel)
+        {
+            if (productModel.ManufacturingDate > productModel.DueDate)
+                throw new InvalidOperationException($"Property {nameof(productModel.ManufacturingDate)} can not be greater than {nameof(productModel.DueDate)} ");
+
+            var productEntity = _mapper.Map<Product>(productModel);
+
+            return await _productsRepository.CreateProductAsync(productEntity);
+        }
+    }
+}
