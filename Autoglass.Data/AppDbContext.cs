17933527171eaed994c3df9fa77bfc50bb2377fa
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:33:58 -0500

    chore: create db context

---
+++ Autoglass.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Autoglass.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Autoglass.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
