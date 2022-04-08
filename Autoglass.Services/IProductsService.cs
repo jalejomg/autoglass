bc7da95fff9c5d8a4f2814c9d023ec506f7ec1e2
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:35:53 -0500

    feat: create contract to manage products

---
+++ Autoglass.Services/IProductsService.cs
@@ -0,0 +1,17 @@
+﻿using Autoglass.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Autoglass.Services
+{
+    public interface IProductsService
+    {
+        Task<ProductModel> GetByIdAsync(int id);
+        Task<List<ProductModel>> GetAllAsync(int amount, int page, string criteria, DateTime manufacturingDateStart, DateTime manufacturingDateEnd,
+            DateTime dueDateStart, DateTime dueDateEnd, string supplier);
+        Task<int> CreateAsync(ProductModel productModel);
+        Task UpdateAsync(ProductModel productModel);
+        Task DeleteAsync(int id);
+    }
+}
