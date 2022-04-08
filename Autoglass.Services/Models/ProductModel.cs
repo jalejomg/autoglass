0cf77a0f416a9aa541e069c334d6c349fda79b06
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:35:03 -0500

    chore: create model to show data to clients

---
+++ Autoglass.Services/Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Autoglass.Services.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime ManufacturingDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public int SupplierId { get; set; }
+
+        public string SupplierDescription { get; set; }
+
+        public string SupplierPhoneNumber { get; set; }
+    }
+}
