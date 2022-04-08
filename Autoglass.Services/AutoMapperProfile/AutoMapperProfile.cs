6d983b7b9cbc6c14254c570a584b0cf5a1da3377
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:37:10 -0500

    chore: implement auto mapper

---
+++ Autoglass.Services/AutoMapperProfile/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Autoglass.Data.Entities;
+using Autoglass.Services.Models;
+using AutoMapper;
+
+namespace Autoglass.Services.AutoMapperProfile
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Product, ProductModel>();
+            CreateMap<ProductModel, Product>();
+        }
+    }
+}
