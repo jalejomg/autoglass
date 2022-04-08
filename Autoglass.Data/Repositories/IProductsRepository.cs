63623dbcc97c2405140ce7fb1537d69af49e78c7
Alejandro Martínez <jalejomg@outlook.com>
2022-04-08 16:32:02 -0500

    feat: create contract with methods declarations to access data

---
+++ Autoglass.Data/Repositories/IProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Autoglass.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Autoglass.Data.Repositories
+{
+    public interface IProductsRepository
+    {
+        Task<int> CreateProductAsync(Product product);
+        Task<IList<Product>> GetAllProductsAsync(int amount, int page, string criteria, DateTime ManufacturingDateStart, DateTime ManufacturingDateEnd,
+            DateTime DueDateStart, DateTime DueDateEnd, string supplier);
+        Task<Product> GetProductByIdAsync(int id);
+        Task UpdateProductAsync(Product product);
+        Task<bool> ProductExistsAsync(int id);
+        Task DeleteProductAsync(Product product);
+    }
+}
